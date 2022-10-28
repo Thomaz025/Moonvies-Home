3a618190f4bfd0b85680641d7f1bf16f38e69264
Thomaz025 <116024577+Thomaz025@users.noreply.github.com>
2022-10-28 18:09:18 -0300

    Add files via upload

---
+++ prototipo/script.js
@@ -0,0 +1,26 @@
+function openBurguer()
+{
+    var menu = document.querySelector('#nav-bar');
+
+    if(menu.style.display === 'none'){
+        document.querySelector('#nav-bar').style.display = 'flex';
+    }
+    else{
+        document.querySelector('#nav-bar').style.display = 'none';
+    }
+}
+
+function openSearch()
+{
+
+    var search = document.querySelector('#search');
+
+    if(menu.style.display === 'none'){
+        document.querySelector('#search').style.display = 'flex';
+
+    }else{
+        document.querySelector('#search').style.display = 'none';
+
+    }
+
+}
